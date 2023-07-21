2bf9e60d1f300f286c8bb468bba6964377f7347b
Colty1999 <mateuszgietka99@gmail.com>
2023-07-21 13:36:43 +0200

    Custom BiCGStab algorithm added

---
+++ BiCGStab-custom.jl
@@ -0,0 +1,31 @@
+function custom_BICGStab(A, b, M, x, tol)
+    r = b - A * x
+    p = 0
+    r_d = r
+    ro = 1
+    w = 1
+    a = 1
+    v = 0
+    for i = 1:2000
+        ro_p = -w * ro
+        ro = transpose(r) * r_d
+        beta = a * ro / ro_p
+        p = r .- beta * (p - w * v)
+        p_d = M \ p
+        v = A * p_d
+        sigma = transpose(v) * r_d
+        a = ro / sigma
+        s = r - a * v
+        s_d = zero(s)
+        ldiv!(s_d, M, s)
+        t = A * s_d
+        w = (transpose(s) * t) / (transpose(t) * t)
+        x = x + a * p_d + w * s_d
+        r = s - w * t
+        if norm(b - A * x) <= tol
+            # print(x)
+            return x
+        end
+    end
+    return x
+end
--- BiCGStab-solver.jl
+++ BiCGStab-solver.jl
@@ -4,51 +4,70 @@ using SparseArrays, LinearOperators, LinearAlgebra
 include("./problems/nonsymmetric-problem-fvm.jl")
 include("./problems/symmetric-problem-fdm.jl")
 include("./problems/symmetric-problem-fem.jl")
+include("./BiCGStab-custom.jl")
 
 function fdm(n)
+    print("FDM\n")
     A, b = fdmproblem(n)
     LU = IncompleteLU.ilu(A)
-    itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+
+
+    x = zeros(size(b))
+    @time bcg_wyn = custom_BICGStab(A, b, LU, x, 1.0e-8)
+    print(" [Left preconditioning] custom_BICGStab Residual norm: ", norm(b - A * bcg_wyn), "\n")
+
+
+    @time itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
 
     n = size(b, 1)
     opP = LinearOperator(Float64, n, n, false, false, (y, v) -> ldiv!(y, LU, v))
-    krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
+    @time krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
+    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n")
 
-    print("FDM\n")
-    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
-    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n\n")
 end
 
 function fem(n)
+    print("FEM\n")
     A, b = femproblem(n)
     LU = IncompleteLU.ilu(A)
-    itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+
+
+    x = zeros(size(b))
+    @time bcg_wyn = custom_BICGStab(A, b, LU, x, 1.0e-8)
+    print(" [Left preconditioning] custom_BICGStab Residual norm: ", norm(b - A * bcg_wyn), "\n")
+
+
+    @time itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
 
     n = size(b, 1)
     opP = LinearOperator(Float64, n, n, false, false, (y, v) -> ldiv!(y, LU, v))
-    krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
-
-    print("FEM\n")
-    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
-    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n\n")
+    @time krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
+    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n")
 end
 
 function fvm()
     f = zeros(1000)
     f[200:300] .= 1.0
+    print("FVM\n")
     A, b = fvmproblem(f)
     LU = IncompleteLU.ilu(A)
-    itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+
+
+    x = zeros(size(b))
+    @time bcg_wyn = custom_BICGStab(A, b, LU, x, 1.0e-8)
+    print(" [Left preconditioning] custom_BICGStab Residual norm: ", norm(b - A * bcg_wyn), "\n")
+
+
+    @time itSol = IterativeSolvers.bicgstabl(A, b, Pl=LU)
+    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
 
     n = size(b, 1)
     opP = LinearOperator(Float64, n, n, false, false, (y, v) -> ldiv!(y, LU, v))
-    krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
-
-    print("FVM\n")
-    print(" [Left preconditioning] IterativeSolvers Residual norm: ", norm(b - A * itSol), "\n")
-    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n\n")
+    @time krl, stats = Krylov.bicgstab(A, b, history=true, M=opP)
+    print(" [Left preconditioning] Krylov residual norm: ", norm(b - A * krl), "\n")
 end
-
 n = 10
 fdm(n)
 fem(n)
